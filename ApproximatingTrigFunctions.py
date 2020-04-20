e1e7b0e946bfeb7db4303fd548df2ec3eff7b84b
aditya-321 <63871262+aditya-321@users.noreply.github.com>
2020-04-20 10:24:26 -0700

    Update ApproximatingTrigFunctions.py

--- ApproximatingTrigFunctions.py
+++ ApproximatingTrigFunctions.py
@@ -1,4 +1,6 @@
 import numpy as np;
+import math;
+PI = np.pi;    
 
 def printIntro():
     print("This program finds the values of trigonometric functions.");
@@ -17,6 +19,15 @@ def getChoice():
 def getAngle():
     return float(input("Enter the angle x in radians: "));
 
+def sin(angle):
+    polynomial = np.array([]);
+    for i in range(15):
+        sign = (-1)**i;
+        coefficient = 1/math.factorial(2*i + 1)
+        degree = 2*i + 1;
+        polynomial = np.append(polynomial, sign*coefficient*angle**degree);
+    approximation = sum(polynomial);
+    return approximation;
 
 printIntro();
 getChoice();
