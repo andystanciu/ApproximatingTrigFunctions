67056f629a22865ceb2a3c32332d9328523803f6
andystanciu <63871223+andystanciu@users.noreply.github.com>
2020-04-23 10:52:52 -0700

    Add files via upload

--- ApproximatingTrigFunctions.py
+++ ApproximatingTrigFunctions.py
@@ -1,13 +1,13 @@
 import numpy as np;
 import math;
-PI = np.pi;    
+PI = np.pi;
+CHOICES = ["sin", "cos", "tan"];
 
 def printIntro():
     print("This program finds the values of trigonometric functions.");
     print("1. sin(x)");
     print("2. cos(x)");
     print("3. tan(x)");
-
 
 def getChoice():
     choice = 0;
@@ -19,16 +19,37 @@ def getChoice():
 def getAngle():
     return float(input("Enter the angle x in radians: "));
 
+def calculateFunction(choice, angle):
+    if (choice == 1):
+        return sin(angle);
+    elif (choice == 2):
+        return cos(angle);
+    else:
+        return tan(angle);
+
 def sin(angle):
     polynomial = np.array([]);
     for i in range(15):
         sign = (-1)**i;
-        coefficient = 1/math.factorial(2*i + 1)
+        coefficient = 1/math.factorial(2*i + 1);
         degree = 2*i + 1;
         polynomial = np.append(polynomial, sign*coefficient*angle**degree);
     approximation = sum(polynomial);
     return approximation;
 
+def cos(angle):
+    return PI/2 - sin(angle);
+
+def tan(angle):
+    threshold = 0.001;
+    if (abs(PI/2 - angle) < threshold):
+        print("The tangent of this angle is undefined.");
+    else:
+        return sin(angle)/cos(angle);
+    
+##def getSinReference(angle):
+    
 printIntro();
-getChoice();
-getAngle();
+choice = getChoice();
+angle = getAngle();
+print(CHOICES[choice - 1] + "(" + str(angle) + ") = " + str(calculateFunction(choice, angle)));
